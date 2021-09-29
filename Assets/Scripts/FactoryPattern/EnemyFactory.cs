487996720b24a3c36ab77e58af725440636fb26a
yennisef <yongrhee1998@gmail.com>
2021-09-29 18:43:17 +0700

    initial commit

---
+++ Assets/Scripts/FactoryPattern/EnemyFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour, IFactory
+{
+
+    [SerializeField]
+    public GameObject[] enemyPrefab;
+    public Transform[] spawnPoints;
+
+    public GameObject FactoryMethod(int tag)
+    {
+        GameObject enemy = Instantiate(enemyPrefab[tag]);
+        return enemy;
+    }
+}
---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public Text warningText;
+    public float restartDelay = 5f;
+    bool isDead = false;
+
+    Animator anim;
+    float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       if (playerHealth.currentHealth <= 0)
+        {
+            if(!isDead)
+            {
+                anim.SetTrigger("GameOver");
+                isDead = true;
+            }
+            restartTimer += Time.deltaTime;
+
+            if(restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+    }
+}
